afb19b968dd8e13aba2fb26a4cda31a9f89dd093
austincunningham <austincunningham@hotmail.com>
2017-04-10 15:27:06 +0100

    keycloak and express running from local instance of keycloak

---
+++ app.js
@@ -0,0 +1,53 @@
+/**
+ * Created by acunningham on 07/04/17.
+ */
+
+'use strict';
+
+const Keycloak = require('keycloak-connect');
+const express = require('express');
+const session = require('express-session');
+const expressHbs = require('express-handlebars');
+const fs = require('fs');
+
+const app = express();
+
+
+// Register 'handelbars' extension with The Mustache Express
+app.engine('hbs', expressHbs({extname:'hbs',
+  defaultLayout:'layout.hbs',
+  relativeTo: __dirname}));
+app.set('view engine', 'hbs');
+
+
+
+
+//session
+app.use(session({
+  secret:'thisShouldBeLongAndSecret',
+  resave: false,
+  saveUninitialized: true,
+  store: memoryStore
+}));
+
+var memoryStore = new session.MemoryStore();
+var keycloak = new Keycloak({ store: memoryStore });
+
+
+app.use(keycloak.middleware());
+
+//route protected with Keycloak
+app.get('/test', keycloak.protect(), function(req, res){
+  res.render('test', {title:'Test of the test'});
+});
+
+//unprotected route
+app.get('/',function(req,res){
+  res.render('index');
+});
+
+app.use( keycloak.middleware( { logout: '/'} ));
+
+app.listen(8000, function () {
+  console.log('Listening at http://localhost:8000');
+});
